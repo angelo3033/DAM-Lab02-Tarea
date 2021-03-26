7f10e2a15d353b9dafef62d5bf53525e0ee4cf16
angel samanez <angel.samanez@tecsup.edu.pe>
2021-03-25 23:52:14 -0500

    diseño de login terminado

--- App.js
+++ App.js
@@ -1,112 +1,81 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, {Component} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  StyleSheet, 
+  TouchableOpacity, 
+  Text, 
+  TextInput,
+  View, 
+  Image} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default class App extends Component{
+  constructor(props) {
+    super(props) ;
+    this.state = {
+      textValue: '',
+      textValue2: '',
+    };
+  }
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  changeTextInput = text =>{
+    this.setState({textValue: text});
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+  changeTextInput2 = text =>{
+    this.setState({textValue2: text});
+  };
 
+  render() {
+    return (
+      <View class="App" style={styles.container}>
+        <Image style={styles.image} source={require('./img/logo.png')}/>
+
+        <View style={styles.text}>
+          <Text> Username </Text>
+        </View>
+        <TextInput
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={text => this.changeTextInput(text)}
+          value={this.state.textValue}
+        />
+
+        <View style={styles.text}>
+          <Text> Password </Text>
+        </View>
+        <TextInput
+          secureTextEntry={true}
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={text => this.changeTextInput2(text)}
+          value={this.state.textValue2}
+        />
+
+        <TouchableOpacity style={styles.button}>
+          <Text> Login </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+    backgroundColor: 'rgba(50, 50, 50, 0.4)',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  text: {
+    alignItems: 'center',
+    padding: 10,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  image: {
+    marginLeft: 70,
+    width: 200,
+    height: 200,
   },
-  highlight: {
-    fontWeight: '700',
+
+  button: {
+    top: 10,
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
   },
 });
-
-export default App;
